df057241ce6bdc9bc7ffef13ddc74a911b157818
zhtick <747109913@qq.com>
2018-05-14 20:53:50 +0800

    数据预处理部分代码，已把每天最接近16点的数据取出

--- lwyc/run/preprocessdata.py
+++ lwyc/run/preprocessdata.py
@@ -7,7 +7,7 @@ import pandas as pd
 from pandas import DataFrame
 import numpy as np
 from sklearn import linear_model
-from tools.db import readdb
+from tools.db import *
 from datetime import datetime
 import re
 # 取主表数据，日期时间，仓温外温
@@ -18,13 +18,15 @@ def maindata():
     CC_LQJKQK_JCSJ AS time,
     CC_LQJKQK_CFNWD AS in_bin,
     CC_LQJKQK_CFWWD AS out_bin
-    FROM CC_LQJKQK WHERE CC_LQJKQK_JCSJ LIKE '16:%'  '''  # +'limit 10'
+    FROM CC_LQJKQK  '''  # +'limit 10'
     main_tuples = readdb(cmd)
     print('The length of main for all is {}!'.format(len(main_tuples)))
-    mdf = DataFrame(list(main_tuples))
-    mdf.columns = ['jknm', 'cm', 'rq', 'sj', 'cw', 'ww']
-    return mdf
-# 取明细表数据，层，列，粮温，将粮温用#分别开，识别每一行的粮温
+    main = DataFrame(list(main_tuples))
+    main.columns = ['jknm', 'cm', 'rq', 'sj', 'cw', 'ww']
+    main.to_csv('{}main.csv'.format(PATH_DATA), encoding='utf-8')
+    return main
+# 取明细表数据，层，列，粮温
+
 def detaildata():
     cmd = '''SELECT CC_LQJKQKMX_JKNM AS jknm,
              CC_LQJKQKMX_JKCJ AS level,
@@ -34,42 +36,100 @@ def detaildata():
     '''
     detail_tuple = readdb(cmd)
     print('The length of main for all is {}!'.format(len(detail_tuple)))
-    ddf = DataFrame(list(detail_tuple))
-    ddf.columns=['jknm', 'ceng', 'lie', 'lw']
-    ddf.set_index(['jknm', 'ceng', 'lie'])
-    split = ddf['lw'].apply(lambda x: pd.Series(x.split("#")))
-    split.columns = [x + 1 for x in split.columns]
-    ddf = ddf.join(split)
-    ddf = ddf.drop('lw', axis=1)
-    return ddf
-# 两表做连接，然后取出每天最接近16:00的那条数据
-def getZhData():
-    main = maindata()
-    detail = detaildata()
-    total = pd.merge(main, detail)
-    total['rq'] = total['rq'].map(lambda x: str(x))
-    total['sj'] = total['sj'].map(lambda x: str(x))
-    total['sj'] = total['sj'].str[7:15]  # 这个是中纺的格式，其它地方注意修改
-    total['rq'] = total['rq']+total['sj']
-    total['rq'] = total['rq'].map(lambda x: datetime.strptime(x, "%Y%m%d%H:%M:%S"))
-    total.drop('sj', axis=1)
-    total.set_index('rq')
-    day_range = pd.date_range(total.index[0].date(), total.index[-1].date(), freq='D')
-    total_16 = DataFrame()
-    for day in day_range:
-        ds_day = total[day.strftime("%Y-%m-%d")]
-        if len(ds_day) > 1 and (len(set(ds_day.index.hour) & {16, 15, 17}) > 0):
-            for i in (16, 15, 17):
-                if(len(ds_day[ds_day.index.hour == i]) > 0):
-                    if(i == 16):
-                        total_16.append(ds_day[ds_day.index.hour == i].iloc[0])
+    detail = DataFrame(list(detail_tuple))
+    detail.columns=['jknm', 'ceng', 'lie', 'lw']
+    detail.to_csv('{}detail.csv'.format(PATH_DATA), encoding='utf-8')
+
+# 数据分仓并清洗
+def fcAndClean():
+    maindata()
+    detaildata()
+    main = pd.read_csv('{}main.csv'.format(PATH_DATA), index_col=0)
+    detail = pd.read_csv('{}detail.csv'.format(PATH_DATA), index_col=0)
+    for meta_id in BIN.index:  # 仓号
+        main_bin = main[main.cm == BIN.loc[meta_id].cname]
+        main_bin.to_csv('{}all/fc/{}main.csv'.format(PATH_DATA, BIN.loc[meta_id].ename), encoding='utf-8')
+        # 根据主表来找对应的明细表
+        tuo = main_bin[['jknm']]
+        detail_bin = pd.merge(detail, tuo)  # , left_on='CC_LQJKQKMX_JKNM', right_on='CC_LQJKQKMX_JKNM')
+        detail_bin.to_csv('{}all/fc/{}detail.csv'.format(PATH_DATA, BIN.loc[meta_id].ename), encoding='utf-8')
+        print('This is {} and we leave at {}!'.format(BIN.loc[meta_id].cname, datetime.now()))
+    for meta_id in range(1, len(BIN) + 1):
+        main = pd.read_csv('{}all/fc/{}main.csv'.format(PATH_DATA, BIN.loc[meta_id].ename), index_col=0)
+        detail = pd.read_csv('{}all/fc/{}detail.csv'.format(PATH_DATA, BIN.loc[meta_id].ename), index_col=0)
+        #主表清洗
+        main['sj'] = main['sj'].str[7:15]  # 这个是中纺的格式，其它地方注意修改
+        main['rq'] = main['rq'].map(lambda x: str(x))  # 后面这几句话的意思是把时间取成时间格式，然后设置成索引
+        main['rq'] = main['rq'] + main['sj']
+        main = main.drop('sj', axis=1)
+        main['rq'] = main['rq'].map(lambda x: datetime.strptime(x, "%Y%m%d%H:%M:%S"))
+        main = main.set_index('rq')
+        main = main[main.cw < FAIR['temp_high']]
+        main = main[main.cw > FAIR['temp_low']]
+        main = main[main.cw != 0]
+        main = main[main.ww < FAIR['temp_high']]
+        main = main[main.ww > FAIR['temp_low']]
+        main = main[main.ww != 0]
+        #明细表清洗
+        temp = main[['jknm']]
+        detail = pd.merge(detail, temp)
+        detail = detail.set_index(['jknm', 'ceng', 'lie'])
+        split = detail['lw'].apply(lambda x: pd.Series(x.split("#")))
+        split.columns = [x + 1 for x in split.columns]
+        detail = detail.join(split)
+        detail = detail.drop('lw', axis=1)
+        detail = detail.stack()
+        detail.index.names = ['jknm', 'level', 'col', 'row']
+        detail = detail.unstack()
+        temp1 = list(range(BIN.loc[meta_id].left, BIN.loc[meta_id].right + 1))  # 取有效的数据列
+        detail = detail[temp1]
+        # 去掉无效值
+        detail = detail.stack()
+        detail = detail.map(lambda x: float(x))
+        detail[detail.values > FAIR['temp_high']] = 0
+        detail[detail.values < FAIR['temp_low']] = 0
+        detail[detail.values == 0] = pd.NaT
+        detail = detail.unstack(['level', 'col', 'row'])
+        detail = detail.stack(['level', 'col', 'row'])
+        detail.name = 'lw'
+        detail = DataFrame(detail)
+        detail.to_csv('{}{}_fair_detail.csv'.format(PATH_DATA, BIN.loc[meta_id].ename), encoding='utf-8')
+        main.to_csv('{}{}_fair_main.csv'.format(PATH_DATA, BIN.loc[meta_id].ename), encoding='utf-8')
+
+#对每仓的数据进行处理，取出每天最接近16:00的那条数据
+def fc16Data():
+    for meta_id in BIN.index:
+        fc_16 = DataFrame()
+        fc_data = pd.read_csv('{}{}_fair_main.csv'.format(PATH_DATA, BIN.loc[meta_id].ename), index_col='rq', parse_dates=True)
+        day_range = pd.date_range(fc_data.index[0].date(), fc_data.index[-1].date(), freq='D')
+        for day in day_range:
+            ds_day = fc_data[day.strftime("%Y-%m-%d")]
+            if len(ds_day) > 1 and (len(set(ds_day.index.hour) & {16, 15, 17}) > 0):
+                for i in (16, 15, 17):
+                    if( len(ds_day[ds_day.index.hour == i]) > 0 and i == 16):
+                        fc_16 = fc_16.append(ds_day[ds_day.index.hour == i].iloc[0])
                         break
-                    if(i == 15):
-                        total_16.append(ds_day[ds_day.index.hour == i].iloc[-1])
+                    if(len(ds_day[ds_day.index.hour == i]) > 0 and i == 15):
+                        fc_16 = fc_16.append(ds_day[ds_day.index.hour == i].iloc[-1])
                         break
-                    if (i == 17):
-                        total_16.append(ds_day[ds_day.index.hour == i].iloc[0])
+                    if (len(ds_day[ds_day.index.hour == i]) > 0 and i == 17):
+                        fc_16 = fc_16.append(ds_day[ds_day.index.hour == i].iloc[0])
                         break
-    return total_16
+        fc_16.to_csv('{}/fc16/{}_16_main.csv'.format(PATH_DATA, BIN.loc[meta_id].ename), encoding='utf-8')
+    for meta_id in BIN.index:
+        fc_detail = pd.read_csv('{}{}_fair_detail.csv'.format(PATH_DATA, BIN.loc[meta_id].ename))
+        fc_main = pd.read_csv('{}/fc16/{}_16_main.csv'.format(PATH_DATA, BIN.loc[meta_id].ename))
+        temp = fc_main[['jknm']]
+        fc_detail = pd.merge(fc_detail, temp)
+        fc_detail.to_csv('{}/fc16/{}_16_detail.csv'.format(PATH_DATA, BIN.loc[meta_id].ename), encoding='utf-8')
+    print('end')
+# fc16对应的ddf，求△值
+def fc16Ddf():
+    for meta_id in BIN.index:
+        fc_detail = pd.read_csv('{}{}_fair_detail.csv'.format(PATH_DATA, BIN.loc[meta_id].ename), index_col=0)
+        fc_main = pd.read_csv('{}/fc16/{}_16_main.csv'.format(PATH_DATA, BIN.loc[meta_id].ename), index_col=0)
+        temp = fc_main[['jknm']]
+        fc_detail = pd.merge(fc_detail, temp)
+        fc_detail.to_csv('{}/fc16/{}_16_detail.csv'.format(PATH_DATA, BIN.loc[meta_id].ename), encoding='utf-8')
 
 
--- lwyc/tools/db.py
+++ lwyc/tools/db.py
@@ -2,6 +2,7 @@
 通用的程序
 '''
 import pymysql
+from pandas import DataFrame
 
 def readdb(cmd, server= "10.24.18.101", user= "root", passwd= "lcwkd123", db= "zfdb"):
     connection = pymysql.connect(server, user, passwd, db, charset="GBK")
@@ -11,3 +12,31 @@ def readdb(cmd, server= "10.24.18.101", user= "root", passwd= "lcwkd123", db= "z
     cursor.close()
     connection.close()
     return results
+DEPOT='ZhongFang'
+PATH_DATA = 'D:/ZhongFang/idata/corn_info/{}/'.format(DEPOT)
+
+# 仓元数据
+# 仓元数据
+BIN = DataFrame({'cname': ['1号仓', '2号仓', '3号仓', '4号仓', '5号仓', '6号仓', '7号仓', '8号仓'],
+                 'ename': ['Bin1', 'Bin2', 'Bin3', 'Bin4', 'Bin5', 'Bin6', 'Bin7', 'Bin8'],
+                 'code': ['ff8080814ac20b70014ac23b62350006', 'ff8080814ac20b70014ac23d15230008',
+                          'ff8080814ac20b70014ac23ea9e9000a', 'ff8080814ac20b70014ac23ff784000c',
+                          'ff8080814ac20b70014ac2413f0c000e', 'ff8080814ac20b70014ac242834b0010',
+                          'ff8080814ac20b70014ac24402b10012', 'ff8080814ac20b70014ac2454d750014'],
+                 'top': [4, 4, 4, 4, 4, 4, 4, 4],
+                 'bottom': [1, 1, 1, 1, 1, 1, 1, 1],
+                 'left': [4, 4, 13, 13, 13, 13, 1, 1], # 一号仓从第四行开始有数据
+                 'right': [23, 23, 23, 23, 23, 23, 23, 23],# 一共有23行
+                 'near': [1, 1, 1, 1, 1, 1, 1, 1],
+                 'far': [5, 5, 5, 5, 5, 5, 5, 5]},
+                index=[1, 2, 3, 4, 5, 6, 7, 8])
+
+# 最高最低的合理数值
+FAIR = {'temp_high': 60, 'temp_low': -40}
+
+# 有粮阶段，meta_id * 10 + 1 是起始时间，meta_id * 10 + 2 是结束时间
+HAS_CORN = {11: '2015-06', 12: '2015-08', 31: '2015-12', 32: '2017-05',
+            41: '2015-12', 42: '2017-05', 51: '2015-04', 52: '2017-05',
+            61: '2015-04', 62: '2017-05', 71: '2015-02', 72: '2017-05'}
+
+
